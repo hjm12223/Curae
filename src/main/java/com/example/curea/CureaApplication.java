83d90d2437fd6e431e93519d2fcafba6e646164a
HongJongMin <gwa7417@gmail.com>
2025-08-22 18:39:49 +0900

    feat: 초기 셋팅

---
+++ src/main/java/com/example/curea/CureaApplication.java
@@ -0,0 +1,13 @@
+package com.example.curea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CureaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CureaApplication.class, args);
+	}
+
+}
