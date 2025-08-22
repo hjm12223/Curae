5c7e3327bfa37a26b3fe8606878fe728a524b8f1
HongJongMin <gwa7417@gmail.com>
2025-08-22 19:44:45 +0900

    feat: User Domain 기초 셋팅

---
+++ src/main/java/com/example/curea/User/Entity/User.java
@@ -0,0 +1,11 @@
+package com.example.curea.User.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private Long id;
+
+}
---
+++ src/main/java/com/example/curea/User/service/UserService.java
@@ -0,0 +1,9 @@
+package com.example.curea.User.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
